3ddb5840f47fdee14fbebfb67c729630deafe6bc
nyro <cedric@nyrodev.com>
2014-07-09 10:44:48 +0200

    Add RawHtmlType

---
+++ Form/Type/RawHtmlType.php
@@ -0,0 +1,13 @@
+<?php
+namespace NyroDev\UtilityBundle\Form\Type;
+
+class RawHtmlType extends AbstractType {
+	
+	public function getParent() {
+		return 'textarea';
+	}
+	
+	public function getName() {
+		return 'rawhtml';
+	}
+}
