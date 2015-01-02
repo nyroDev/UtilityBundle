8d5712b357b0d1a0431cd4745714666a695aa7b1
nyro <cedric@nyrodev.com>
2015-01-02 20:33:18 +0100

    Add translation ability for form errors

--- Resources/views/Form/form_errors.html.php
+++ Resources/views/Form/form_errors.html.php
@@ -1,7 +1,7 @@
 <?php if ($errors && count($errors)): ?>
     <ul class="form_errors">
         <?php foreach ($errors as $error): ?>
-            <li><?php echo $error->getMessage() ?></li>
+            <li><?php echo $view['nyrodev']->trans($error->getMessage()) ?></li>
         <?php endforeach; ?>
     </ul>
 <?php endif ?>
