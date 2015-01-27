0672343cfe8ecb16e7d06f4cdbc60d5f4191f30d
nyro <cedric@nyrodev.com>
2015-01-27 15:10:20 +0100

    Add data-cancelurl ability for button_row template

--- Resources/views/Form/button_row.html.php
+++ Resources/views/Form/button_row.html.php
@@ -2,5 +2,10 @@
 	<span class="formRequiredFields">
 		<?php echo $view['translator']->trans('nyrodev.required') ?>
 	</span>
-    <?php echo $view['form']->widget($form) ?>
+    <?php
+	echo $view['form']->widget($form);
+	if ($form->vars && isset($form->vars['attr']) && isset($form->vars['attr']['data-cancelurl']) && $form->vars['attr']['data-cancelurl']) {
+		echo '<a href="'.$form->vars['attr']['data-cancelurl'].'" class="button cancel">'.$view['translator']->trans('admin.misc.cancel').'</a>';
+	}
+	?>
 </div>
