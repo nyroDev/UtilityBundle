22dedfe633324d9c41ca1a7b6bd1b6f245f38313
nyro <cedric@nyrodev.com>
2015-03-20 16:35:50 +0100

    Update responsive File manager to version 9.2.2

--- Resources/views/filemanager/config/config.php
+++ Resources/views/filemanager/config/config.php
@@ -4,8 +4,7 @@
 //session_start();
 
 mb_internal_encoding('UTF-8');
-//------------------------------------------------------------------------------
-// DON'T COPY THIS VARIABLES IN FOLDERS config.php FILES
-//------------------------------------------------------------------------------
 
 // Clean for nyroDev/Utility-bundle, variuable settigns are in the Controller
+
+return $config;
