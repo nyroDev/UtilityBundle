74c762c7c9c7950d8d4836c92174a8ea2e3a1392
nyro <cedric@nyrodev.com>
2015-04-08 22:32:10 +0200

    Add TransparentPixelResponse and PhpExcelResponse

---
+++ Utility/PhpExcelResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NyroDev\UtilityBundle\Utility;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+/**
+ * Class PhpExcelResponse
+ */
+class PhpExcelResponse extends StreamedResponse {
+
+	public function setPhpExcel($filename, \PHPExcel $phpExcel, $phpExcelFormat = 'Excel2007') {
+		$this->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		$this->headers->set('Content-Disposition', 'attachment;filename="'.$filename.'"');
+		$this->setPrivate();
+		$this->headers->addCacheControlDirective('no-cache', true);
+		$this->headers->addCacheControlDirective('must-revalidate', true);
+		
+		$objWriter = \PHPExcel_IOFactory::createWriter($phpExcel, $phpExcelFormat);
+		$this->setCallback(function() use ($objWriter) {
+			$objWriter->save('php://output');
+		});
+	}
+
+}
