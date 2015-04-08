4d2a36f571d3844a7e94f8bbc2583f11efb454b0
nyro <cedric@nyrodev.com>
2015-04-08 22:36:51 +0200

    Add TcpdfResponse

---
+++ Utility/TcpdfResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NyroDev\UtilityBundle\Utility;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class TcpdfResponse
+ */
+class TcpdfResponse extends Response {
+
+	public function setTcpdf(\TCPDF $tcpdf, $fileDownload = false) {
+		$this->headers->set('Content-Type', 'application/pdf');
+		$this->setContent($tcpdf->Output('export.pdf', 'S'));
+		
+		if ($fileDownload) {
+			$this->headers->set('Content-Disposition', 'attachment;filename="'.$fileDownload.'"');
+			$this->setPrivate();
+			$this->headers->addCacheControlDirective('no-cache', true);
+			$this->headers->addCacheControlDirective('must-revalidate', true);
+		}
+	}
+
+}
