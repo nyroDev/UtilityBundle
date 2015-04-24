c17dc3a0ac9d0ffad79192e1862d6aa643a23b3a
nyro <cedric@nyrodev.com>
2015-04-24 14:28:45 +0200

    Add plupload functionnality

---
+++ Resources/public/js/plupload/100_nyroPlupload.js
@@ -0,0 +1,139 @@
+jQuery(function($) {
+	var pluploadNb = 0;
+	$.fn.extend({
+		nyroPlupload: function(opts) {
+			return this.each(function() {
+				var me = $(this).hide(),
+					myPluploadNb = pluploadNb++,
+					curOpts = opts,
+					cont = $('<div id="pluploadCont'+myPluploadNb+'" class="pluploadCont" />').insertAfter(me),
+					curFiles = {},
+					browse = $('<a href="#" id="pluploadBrowse'+myPluploadNb+'" class="pluploadBrowse">'+curOpts.texts.browse+'</a>').appendTo(cont);
+				
+				if (opts.showCancelAll)
+					var cancelAll = $('<a href="#" id="pluploadCancelAll'+myPluploadNb+'" class="pluploadCancelAll">'+curOpts.texts.cancelAll+'</a>').appendTo(cont).hide();
+					
+				var list = $('<div id="pluploadList'+myPluploadNb+'" class="pluploadList" />').appendTo(cont);
+				
+				curOpts.file_data_name = me.attr('name');
+				curOpts.container = 'pluploadCont'+myPluploadNb;
+				curOpts.drop_element = 'pluploadCont'+myPluploadNb;
+				curOpts.browse_button = 'pluploadBrowse'+myPluploadNb;
+				if (!curOpts.url)
+					curOpts.url = me.closest('form').attr('action');
+				if (!curOpts.url)
+					curOpts.url = document.location.href;
+				
+				var uploader = new plupload.Uploader(curOpts);
+				uploader.bind('FilesAdded', function(up, files) {
+					for (var i in files) {
+						var curFile = files[i],
+							name = curFile.name;
+						if (name.length > 30)
+							name = name.substr(0, 30) + '...'
+						curFiles[curFile.id] = $('<div>'+name+' (' + plupload.formatSize(curFile.size) + ') - <strong>'+curOpts.texts.waiting+'</strong><div class="pluploadProgress"><div class="pluploadProgressBar"></div></div><a href="#" class="pluploadCancel" rel="'+curFile.id+'">Cancel</a></div>');
+						curFiles[curFile.id].find('.pluploadCancel').on('click', function(e) {
+							e.preventDefault();
+							uploader.removeFile(uploader.getFile($(this).attr('rel')));
+						});
+						list.append(curFiles[curFile.id]);
+					}
+					if (opts.showCancelAll)
+						cancelAll.show();
+					setTimeout(function() {uploader.start();}, 1);
+				});
+				if (opts.showCancelAll) {
+					cancelAll.on('click', function(e) {
+						e.preventDefault();
+						cont.find('.pluploadCancel').trigger('click');
+					});
+				}
+				if (opts.addFormVars) {
+					var form = me.closest('form');
+					uploader.bind('BeforeUpload', function(up, file) {
+						var tmp = {};
+						$.each(form.serializeArray(), function(index, val) {
+							tmp[val.name] = val.value;
+						});
+						up.settings.multipart_params = tmp;
+					});
+				}
+				uploader.bind('UploadProgress', function(up, file) {
+					if (curFiles[file.id] && !isNaN(file.percent)) {
+						curFiles[file.id]
+							.children('strong').text(file.percent+' %').end()
+							.find('.pluploadProgressBar').css('width', file.percent+'%');
+					}
+				});
+				uploader.bind('FileUploaded', function(up, file) {
+					if (curFiles[file.id]) {
+						curFiles[file.id].children('strong').text(curOpts.texts.complete);
+						curFiles[file.id].delay(curOpts.hideDelay).fadeOut(function() {
+							curFiles[file.id].remove();
+							curFiles[file.id] = undefined;
+							delete(curFiles[file.id]);
+						})
+					}
+				});
+				uploader.bind('FilesRemoved', function(up, files) {
+					for (var i in files) {
+						var file = files[i];
+						if (curFiles[file.id]) {
+							curFiles[file.id].children('strong').text(curOpts.texts.cancel);
+							curFiles[file.id].delay(curOpts.hideDelay * 3).fadeOut(function() {
+								curFiles[file.id].remove();
+								curFiles[file.id] = undefined;
+								delete(curFiles[file.id]);
+							})
+						}
+					}
+				});
+				uploader.bind('Error', function(up, obj) {
+					if (obj.file && curFiles[obj.file.id]) {
+						curFiles[obj.file.id]
+							.addClass('pluploadError')
+							.children('strong').html(curOpts.texts.error+'<br />'+obj.message+(obj.status ? ' ('+obj.status+')' : ''));
+					}
+				});
+				if (opts.showCancelAll)
+					uploader.bind('UploadComplete', function() {cancelAll.hide();});
+				if (curOpts.onAllComplete && $.isFunction(curOpts.onAllComplete)) {
+					uploader.bind('UploadComplete', function() {setTimeout(curOpts.onAllComplete, 20);});
+				}
+				
+				if (curOpts.events) {
+					$.each(curOpts.events, function(k, v) {
+						uploader.bind(k, v);
+					});
+				}
+				
+				uploader.init();
+				me.data('nyroPluploader', uploader);
+			});
+		}
+	});
+	
+	$('form.pluploadInit').each(function() {
+		var me = $(this).addClass('pluploadForm'),
+			input = me.find('input[type="file"]')
+		input.nyroPlupload({
+			showCancelAll: true,
+			addFormVars: true,
+			hideDelay: 750,
+			filters: me.data('plupload_filters'),
+			flash_swf_url: me.data('plupload_swf'),
+			silverlight_xap_url: me.data('plupload_xap'),
+			texts: {
+				browse: me.data('plupload_browse'),
+				waiting: me.data('plupload_waiting'),
+				error: me.data('plupload_error'),
+				cancel: me.data('plupload_cancel'),
+				complete: me.data('plupload_complete'),
+				cancelAll: me.data('plupload_cancelall')
+			},
+			onAllComplete: function() {
+				document.location.reload();
+			}
+		});
+	});
+});
--- Services/FormService.php
+++ Services/FormService.php
@@ -18,5 +18,28 @@ class FormService extends AbstractService {
 			)
 		));
 	}
+	
+	public function getPluploadAttrs($filters = 'images') {
+		if ($filters == 'images') {
+			$filters = array(
+				array(
+					'title'=>$this->trans('nyrodev.plupload.images'),
+					'extensions'=>'jpg,jpeg,gif,png'
+				)
+			);
+		}
+		return array(
+			'class'=>'pluploadInit',
+			'data-plupload_browse'=>$this->trans('nyrodev.plupload.browse'),
+			'data-plupload_waiting'=>$this->trans('nyrodev.plupload.waiting'),
+			'data-plupload_error'=>$this->trans('nyrodev.plupload.error'),
+			'data-plupload_cancel'=>$this->trans('nyrodev.plupload.cancel'),
+			'data-plupload_complete'=>$this->trans('nyrodev.plupload.complete'),
+			'data-plupload_cancelall'=>$this->trans('nyrodev.plupload.cancelAll'),
+			'data-plupload_filters'=>json_encode($filters),
+			'data-plupload_swf'=>$this->get('templating.helper.assets')->getUrl('bundles/nyrodevutility/vendor/plupload/Moxie.swf'),
+			'data-plupload_xap'=>$this->get('templating.helper.assets')->getUrl('bundles/nyrodevutility/vendor/plupload/Moxie.xap'),
+		);
+	}
 
 }
