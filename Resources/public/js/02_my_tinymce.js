4d4577d8affca8c49d15889c757c6bccb06957dd
nyro <cedric@nyrodev.com>
2015-06-26 16:10:34 +0200

    Improve mytinymce JS in order to extract function and make them callable from outside

--- Resources/public/js/02_my_tinymce.js
+++ Resources/public/js/02_my_tinymce.js
@@ -1,4 +1,4 @@
-$(function() {
+jQuery(function($) {
 	var tinymceLoaded = false,
 		tinymceLoading = false,
 		tinymceLoadingQueue = [],
@@ -32,42 +32,54 @@ $(function() {
 		tinymceKey = 'tinymce_',
 		tinymceKeyLn = tinymceKey.length;
 	
-	$('textarea.tinymce').each(function() {
-		var me = $(this),
-			opts = {
-				oninit: function(ed) {
-					me.trigger('tinmceInit', [ed]);
+	$.fn.extend({
+		myTinymceDataSearch: function() {
+			var opts = {};
+			$.each($(this).first().data(), function(i, e) {
+				if (i.indexOf(tinymceKey) == 0) {
+					if (typeof e == 'string' && e.indexOf('function(') == 0) {
+						eval('window.tinyval = '+e+';');
+						e = window.tinyval;
+						delete(window.tinyval);
+					}
+					opts[i.substring(tinymceKeyLn)] = e;
 				}
-			};
-		$.each(me.data(), function(i, e) {
-			if (i.indexOf(tinymceKey) == 0) {
-				if (typeof e == 'string' && e.indexOf('function(') == 0) {
-					eval('window.tinyval = '+e+';');
-					e = window.tinyval;
-					delete(window.tinyval);
-				}
-				opts[i.substring(tinymceKeyLn)] = e;
-			}
-		});
-		tinymceLoad(me.data('tinymceurl'), function() {
-			if (me.data('browser_url')) {
-				opts['file_browser_callback'] = function(field_name, url, type, win) {
-					parent.nyroBrowserField = field_name;
-					parent.nyroBrowserWinBrowse = tinyMCE.activeEditor.windowManager.open({
-						url: me.data('browser_url')+'?type='+type+'&',
-						title: me.data('browser_title'),
-						width: me.data('browser_width'),
-						height: me.data('browser_height'),
-						resizable: true,
-						maximizable: true,
-						scrollbars: true
-					});
-					return false;
-				};
-			}
-			if (me.is('[required]'))
-				me.removeProp('required').removeAttr('required');
-			me.tinymce(opts);
-		});
+			});
+			return opts;
+		},
+		myTinymce: function(options, skipDataSearch) {
+			return this.each(function() {
+				var me = $(this),
+					opts = $.extend({
+						oninit: function(ed) {
+							me.trigger('tinmceInit', [ed]);
+						}
+					}, options);
+				if (!skipDataSearch)
+					$.extend(opts, me.myTinymceDataSearch());
+				tinymceLoad(me.data('tinymceurl'), function() {
+					if (me.data('browser_url')) {
+						opts['file_browser_callback'] = function(field_name, url, type, win) {
+							parent.nyroBrowserField = field_name;
+							parent.nyroBrowserWinBrowse = tinyMCE.activeEditor.windowManager.open({
+								url: me.data('browser_url')+'?type='+type+'&',
+								title: me.data('browser_title'),
+								width: me.data('browser_width'),
+								height: me.data('browser_height'),
+								resizable: true,
+								maximizable: true,
+								scrollbars: true
+							});
+							return false;
+						};
+					}
+					if (me.is('[required]'))
+						me.removeProp('required').removeAttr('required');
+					me.tinymce(opts);
+				});
+			});
+		}
 	});
+	
+	$('textarea.tinymce').myTinymce();
 });
