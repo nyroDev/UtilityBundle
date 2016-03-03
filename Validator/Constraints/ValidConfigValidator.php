12c2e9df5343d7ddbcbe7b2b832c4c865ce1108a
nyro <cedric@nyrodev.com>
2016-03-03 11:40:21 +0100

    Add configType to handle json value and it's validator

---
+++ Validator/Constraints/ValidConfigValidator.php
@@ -0,0 +1,19 @@
+<?php
+namespace NyroDev\UtilityBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * @Annotation
+ */
+class ValidConfigValidator extends ConstraintValidator {
+	
+	public function validate($value, Constraint $constraint) {
+		if ($value === false) {
+			$this->context->buildViolation($constraint->message)
+				->addViolation();	
+		}
+	}
+
+}
