37d1cf1cff065184cdf8619d785e30b8af874bc5
Romain Pichot <romain@pichot.fr>
2016-11-25 16:35:01 +0100

    [ADD] Trigger change event on autocomplete select

--- Resources/public/js/autocomplete/50_autocomplete.js
+++ Resources/public/js/autocomplete/50_autocomplete.js
@@ -142,7 +142,9 @@ jQuery(function($, undefined) {
 					},
 					select: function(event, ui) {
 						this.value = ui.item.label;
-													$(this).trigger('change');
+						$(this).trigger('change');
+						me.val(ui.item.value);
+						me.trigger('change');
 						return false;
 					}
 				});
@@ -157,7 +159,7 @@ jQuery(function($, undefined) {
 			}).end();
 		}
 	});
-	
+
 	$('select.autocompleteSelMul').autocompleteSelMul();
 	$('select.autocompleteSel').autocompleteSel();
 });
