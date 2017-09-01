581e6e58697904e832b80d49d4c893ff8b15e1e6
nyro <cedric@nyrodev.com>
2017-09-01 15:06:08 +0200

    Update to TinyMce 4.6.6 and Responsive File Manager 9.11.3

--- Resources/views/filemanager/config/config.php
+++ Resources/views/filemanager/config/config.php
@@ -1,11 +1,13 @@
 <?php
-
 // Comment for nyroDev/Utility-bundle
 //if (session_id() == '') session_start();
 
 mb_internal_encoding('UTF-8');
-// Comment for nyroDev/Utility-bundle
-//date_default_timezone_set('Europe/Rome');
+mb_http_output('UTF-8');
+mb_http_input('UTF-8');
+mb_language('uni');
+mb_regex_encoding('UTF-8');
+ob_start('mb_output_handler');
 
 // Useful to put variable in global scope
 foreach($configNyro as $k=>$v) {
