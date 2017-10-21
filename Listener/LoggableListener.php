9449e4310e71d2db251bdb498216dfbf9abe5910
nyro <cedric@nyrodev.com>
2017-10-21 11:08:27 +0200

    Fix logable listener to remove change_ when needed

--- Listener/LoggableListener.php
+++ Listener/LoggableListener.php
@@ -9,7 +9,12 @@ class LoggableListener extends SrcLoggableListener
     protected function prePersistLogEntry($logEntry, $object)
     {
         if (method_exists($object, 'getTranslatableLocale') && method_exists($logEntry, 'setLocale') && $object->getTranslatableLocale()) {
-            $logEntry->setLocale($object->getTranslatableLocale());
+            $locale = $object->getTranslatableLocale();
+            if (strpos($locale, 'change_') === 0) {
+                $tmp = explode('change_', $locale);
+                $locale = $tmp[1];
+            }
+            $logEntry->setLocale($locale);
         }
     }
 }
