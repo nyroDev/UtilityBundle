8df2aed806ebb8143d74152f69af634c18f15cf7
nyro <cedric@nyrodev.com>
2019-01-31 10:22:52 +0100

    Add standardizeFileName function

--- Services/MainService.php
+++ Services/MainService.php
@@ -342,15 +342,7 @@ class MainService extends AbstractService
      */
     public function getUniqFileName($dir, $name, $sep = '-')
     {
-        $name = mb_strtolower($name);
-        $ext = $this->getExt($name);
-        if ($ext) {
-            $pos = strpos($name, $ext);
-            if ($pos > 0) {
-                $name = substr($name, 0, $pos);
-            }
-        }
-        $name = $this->urlify($name);
+        list($name, $ext) = $this->standardizeFileName($name, true);
 
         $nameF = $name.'.'.$ext;
         $i = 2;
@@ -361,6 +353,32 @@ class MainService extends AbstractService
         $this->uniqFileNames[$dir.'/'.$nameF] = true;
 
         return $nameF;
+    }
+
+    /**
+     * Standardize filename using urlify function and keeping the extension.
+     *
+     * @param string $name    Original filename
+     * @param bool   $asArray Indicates if the return should be an array or a string
+     *
+     * @return string
+     */
+    public function standardizeFileName($name, $asArray = false)
+    {
+        $name = mb_strtolower($name);
+        $ext = $this->getExt($name);
+        if ($ext) {
+            $pos = strpos($name, $ext);
+            if ($pos > 0) {
+                $name = substr($name, 0, $pos);
+            }
+        }
+        $name = $this->urlify($name);
+
+        return $asArray ? [
+            $name,
+            $ext,
+        ] : $name.'.'.$ext;
     }
 
     /**
