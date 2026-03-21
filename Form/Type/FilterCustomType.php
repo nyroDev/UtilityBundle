2b7b5181155790c9623ac60cf041cb5647b74c04
nyro <cedric@nyro.dev>
2026-03-21 15:39:27 +0100

    Enable applyFilter for all FilterType

--- Form/Type/FilterCustomType.php
+++ Form/Type/FilterCustomType.php
@@ -13,11 +13,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class FilterCustomType extends FilterType
 {
-    protected $applyFilters = [];
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $this->applyFilters[$builder->getName()] = $options['applyFilter'];
         if ($options['showTransformer']) {
             if (isset($options['transformerChoices']) && $options['transformerChoices'] && count($options['transformerChoices'])) {
                 $builder
@@ -39,20 +36,9 @@ class FilterCustomType extends FilterType
         return AbstractQueryBuilder::OPERATOR_EQUALS;
     }
 
-    public function applyFilter(AbstractQueryBuilder $queryBuilder, string $name, array $data): AbstractQueryBuilder
-    {
-        $applyFilter = $this->applyFilters[$name];
-        if (!isset($data['transformer'])) {
-            $data['transformer'] = $this->getDefaultTransformer();
-        }
-
-        return $applyFilter ? $applyFilter($queryBuilder, $name, $data) : $queryBuilder;
-    }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'applyFilter' => null,
             'transformerChoices' => [
                 '=' => AbstractQueryBuilder::OPERATOR_EQUALS,
             ],
