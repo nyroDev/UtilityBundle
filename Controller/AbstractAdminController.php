d289979d71e98e46044f91a4de014425ec65d5ba
nyro <cedric@nyro.dev>
2026-06-13 15:28:48 +0200

    Remvoe usage of strftime to use IntlDateFormatter

--- Controller/AbstractAdminController.php
+++ Controller/AbstractAdminController.php
@@ -102,7 +102,7 @@ abstract class AbstractAdminController extends AbstractController
                     $val = $accessor->getValue($r, $field);
                     if (is_object($val)) {
                         if ($val instanceof DateTimeInterface) {
-                            $val = strftime($this->trans('date.short'), $val->getTimestamp());
+                            $val = $this->get(NyrodevService::class)->formatDate($val, 'date.short');
                         } elseif ($val instanceof Collection) {
                             $val = $this->get(NyrodevService::class)->joinRows($val);
                         } else {
